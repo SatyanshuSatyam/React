187b0c50d3b3011a9976c10a3c13bcb99e5a7c96
SatyanshuSatyam <satyanshu2801@gmail.com>
2024-09-01 10:45:04 +0530

    react navbar

---
+++ test-project/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import NavBar from './Components/Navigation/NavBar';
+
+
+const App = () => {
+  return (
+    <div>
+      <NavBar />
+    </div>
+  )
+}
+
+export default App;
---
+++ test-project/src/Components/Navigation/NavBar.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./NavBar.css";
+
+const NavBar = () => {
+    return (
+        <div className="nav-container">
+            <h2>Satyanshu.</h2>
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Services</li>
+                <li>Cunt-act</li>
+            </ul>
+        </div>
+    )
+}
+
+export default NavBar;
